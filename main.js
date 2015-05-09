726e93b12e88754f52dbb35d4ef91b48a1ffce77
William Dyce <wilbefast@gmail.com>
2015-05-09 05:58:07 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,18 @@
+var express = require('express'); 
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+
+  socket.on('cmd', function(cmd) {
+		console.log('cmd', cmd);
+	})
+});
+
+app.use(express.static("client"));
+
+http.listen(3000, function(){
+  console.log('listening on port 3000');
+});
