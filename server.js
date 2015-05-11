f3eabc827f8fdde9550efe6e25b6048f8221e467
William Dyce <wilbefast@gmail.com>
2015-05-11 23:46:02 +0200

    configuration to work on aws

--- server.js
+++ server.js
@@ -242,6 +242,6 @@ app.use(express.static("client"));
 // HTTP
 // ------------------------------------
 
-http.listen(3000, function(){
+http.listen(process.env.PORT || 3000, function(){
   console.log('listening on port 3000');
 });
